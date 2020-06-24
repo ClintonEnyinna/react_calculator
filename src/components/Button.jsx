3102aaedb567e5721ee690a67d2c871f7604c51d
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-06-24 01:55:44 -0500

    feat: implement Display, Button and ButtonPanel components

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Button({ name }) {
+  return <button type="button">{name}</button>;
+}
+
+export default Button;
---
+++ src/components/Display.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Display({ result }) {
+  return <div>{result}</div>;
+}
+
+export default Display;
