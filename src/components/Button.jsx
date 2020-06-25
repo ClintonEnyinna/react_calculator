2d07d46d883d1e00599ca7a267097d1d636f6171
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-06-25 00:37:44 -0500

    feat: style button, ButtonPanel, and Display

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Button({ name }) {
-  return <button type="button">{name}</button>;
+  return (
+    <button className="btn" type="button">
+      {name}
+    </button>
+  );
 }
 
 Button.propTypes = {
